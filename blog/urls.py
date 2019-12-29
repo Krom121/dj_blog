564eb73054bd5edb37987d02f4504fa3c0c6517a
stephen <stephenmacgregor9@gmail.com>
2019-12-29 14:30:28 +0000

    post view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, blog
+from .views import index, blog, post
 
 
 urlpatterns = [
     path('', index),
     path('blog/', blog),
+    path('post/', post),
 ]
